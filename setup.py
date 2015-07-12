e0f9544b3181d556123b8a2c16c30b887a5f5c49
econokeith <keith.blackwell@gmail.com>
2015-07-12 11:58:36 -0400

    packaging

---
+++ setup.py
@@ -0,0 +1,33 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+long_description = """
+bondfuns : basic price / yield / calendar functions for evaluating US Treasuries:
+
+bondfun creates a basic Treasury bond object Treasury() which follows UST conventions 
+(i.e. Actual/Actual, semi-annual coupons, trades settle T+1 etc). It provides basic methods 
+for solving for price, yield to maturity, accrued interest, etc as well as calendar 
+functions to find trade settles, business days, etc. All methods assume entry of settle date 
+not trade date, but have the options to assume trade date if necessary.
+"""
+
+setup(
+    name="bondfuns",
+    version="0.0",
+    description="bondfuns : basic price / yield / calendar functions for evaluating US Treasuries",
+    long_description=long_description,
+    keywords="finance, bonds, yield to maturity, Treasuries",
+    author="Keith Blackwell",
+    author_email="keith.blackwell@gmail.com",
+    url="https://github.com/econokeith/bondfuns",
+    license="MIT",
+    packages=["bondfuns"],
+    package_data={'bondfuns':['data/*.csv']},
+    install_requires=[ "dateutils"],
+    classifiers=[
+        'Development Status :: Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2'])
